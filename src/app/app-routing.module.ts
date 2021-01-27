6955ed1a43449a46b5d30da8f5f83c0d2272c129
j.miranda <j.miranda@192.168.1.254>
2021-01-27 02:23:50 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { SearchProductComponent } from './views/search-product/search-product.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SearchProductComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { SearchProductComponent } from './views/search-product/search-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SearchProductComponent
   ],
   imports: [
     BrowserModule,
