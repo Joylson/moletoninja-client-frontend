b70d25793440ee70cd931749a11d66635d71bcd1
lucas.assis-at-887246021887 <lucasaassis@outlook.com>
2021-02-15 08:45:09 -0300

    master - ajustando visualizaçao de pedidos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { SearchProductComponent } from './views/search-product/search-product.component';
 import { OrdersComponent } from './views/orders/orders.component';
+import { OrderFormComponent } from './views/order-form/order-form.component';
 import { LoginComponent } from './views/login/login.component';
 
 import { AuthGuard } from './helpers/auth.guard';
@@ -11,6 +12,7 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: '', component: SearchProductComponent },
   { path: 'orders', component: OrdersComponent, canActivate: [AuthGuard] },
+  { path: 'orders/:id', component: OrderFormComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: '' } //redireciona para a home caso a rota não exista
 ];
 
