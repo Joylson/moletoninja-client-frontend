52cab8c389741864e33a97a9fecf6c2b40c9e68b
j.miranda <j.miranda@192.168.1.254>
2021-02-18 02:18:02 -0300

    ajustado selecionar produto de estoque do produto do kit

--- src/app/views/product/product.component.ts
+++ src/app/views/product/product.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChange
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { OrderService } from 'src/app/services/order.service';
 import { ProductService } from 'src/app/services/product.service';
+import { StockProductService } from 'src/app/services/stock-product.service';
 
 @Component({
   selector: 'app-product',
@@ -12,6 +13,8 @@ export class ProductComponent implements OnInit, OnChanges {
 
 
   @Input() id: number;
+
+  @Input() kit: boolean = false;
 
   @Output() add = new EventEmitter<any>();
 
@@ -27,9 +30,16 @@ export class ProductComponent implements OnInit, OnChanges {
 
   public msg: string;
 
+  //kit
+  public productKit: any;
+  public selStockProductKit: any = [];
+
+  public loading: boolean = false;
+
   constructor(private productService: ProductService,
     private modalService: NgbModal,
-    private orderService: OrderService) { }
+    private orderService: OrderService,
+    private stockProductService: StockProductService) { }
 
   ngOnChanges(changes: SimpleChanges): void {
     this.get();
@@ -55,8 +65,15 @@ export class ProductComponent implements OnInit, OnChanges {
     });
   }
 
-  open(content) {
+  openPrint(content) {
     this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title', size: 'xl', centered: true }).result.then((result) => {
+    }, (reason) => {
+    });
+  }
+
+  openKitProduct(content, product) {
+    this.productKit = product;
+    this.modalService.open(content, { ariaLabelledBy: 'modal-basic-title', size: 'lg', centered: true }).result.then((result) => {
     }, (reason) => {
     });
   }
@@ -73,6 +90,25 @@ export class ProductComponent implements OnInit, OnChanges {
     this.msg = null;
     this.orderService.addStockProductColorAndSize(this.selColor.id, this.selSize.id, this.id, this.print);
     this.add.emit(null);
+  }
+
+  public async addKitProduct() {
+    if (!this.selColor) {
+      this.msg = 'não foi selecionado a cor';
+      return;
+    }
+    if (!this.selSize) {
+      this.msg = 'não foi selecionado o tamanho';
+      return;
+    }
+    this.msg = null;
+    this.loading = true;
+    this.stockProductService.getByColorAndSizeAndProduct(this.selColor.id, this.selSize.id, this.id).subscribe((data: any) => {
+      this.loading = false;
+      if (this.print)
+        data.print = this.print;
+      this.add.emit(data);
+    });
   }
 
   public selProduct(product) {
@@ -95,4 +131,35 @@ export class ProductComponent implements OnInit, OnChanges {
     modal.dismiss('Cross click');
   }
 
+  public getStockProduct(product): any {
+    if (this.selStockProductKit) {
+      for (const st of this.selStockProductKit) {
+        if (product.id === st.product.id)
+          return st;
+      }
+      return null;
+    } else
+      return null;
+  }
+
+
+  public addKit(modal, content, stock) {
+    modal.dismiss('Cross click');
+    let valid = true;
+    let i = 0;
+    let index;
+    if (this.selStockProductKit)
+      for (const st of this.selStockProductKit) {
+        i++;
+        if (stock.product.id === st.product.id) {
+          valid = false;
+          index = 1;
+        }
+      }
+    console.log(valid, index)
+    if (valid)
+      this.selStockProductKit.push(stock);
+    else
+      this.selStockProductKit.splice(index - 1, 1, stock);
+  }
 }
